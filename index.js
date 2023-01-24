a9a552e5272b5fe9195f00ccb9f958ef68eebbd2
Benson Tamunoemi <bensontamunoemi@gmail.com>
2023-01-25 00:00:48 +0400

    send text to my babe

---
+++ index.js
@@ -0,0 +1,29 @@
+import cron from 'node-cron';
+import dotenv from 'dotenv';
+import twilio from 'twilio';
+import messages from './messages.js';
+dotenv.config();
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+const currentMessage = 0;
+
+function sendMessage() {
+  client.messages
+    .create({
+      body: messages[currentMessage],
+      from: '+17707623141',
+      to: process.env.PHONE_NUMBER,
+    })
+    .then((message) => {
+      currentMessage++;
+      console.log(message.sid);
+    });
+}
+
+cron.schedule('0 8 * * *', () => {
+  sendMessage();
+  console.log('Message sent!');
+});
